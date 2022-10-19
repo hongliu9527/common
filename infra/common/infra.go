51ef2c4005ef0eb3289326ee35da2e7fab6b8d6e
hongliu9527 <635781500@qq.com>
2022-10-19 14:00:32 +0800

    修复细微bug

--- infra/common/infra.go
+++ infra/common/infra.go
@@ -2,7 +2,7 @@
  * @Author: hongliu
  * @Date: 2022-09-21 15:46:30
  * @LastEditors: hongliu
- * @LastEditTime: 2022-10-18 15:51:40
+ * @LastEditTime: 2022-10-19 14:00:18
  * @FilePath: \common\infra\common\infra.go
  * @Description:基础设施接口定义
  *
@@ -17,7 +17,7 @@ type Infra interface {
 	Start(ctx context.Context) error   // 启动基础设施
 	Stop() error                       // 关闭基础设施
 	Restart(ctx context.Context) error // 重启基础设施
-	Name() string                      // 查询基础设施名称
+	Name() string                      // 基础设施名称
 }
 
 // OrmInfra orm基础设施接口定义
